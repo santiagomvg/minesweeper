6f5b2283c5809183538aeac52093d8329389af93
santiagomvg <santiago.m.vidal.g@gmail.com>
2020-01-03 20:20:25 -0300

    basic html main page

--- static/mineSW_lib.js
+++ static/mineSW_lib.js
@@ -1,4 +1,16 @@
-function MineSW() {}
+function MineSW() {
+    //default callbacks
+    this._errorCallback = function(e) { alert(e); };
+    this._renderCallback = function(board) { alert(board); };
+}
+
+MineSW.prototype.setRenderCallback = function(cb) {
+
+};
+
+MineSW.prototype.setErrorCallback = function(cb) {
+
+};
 
 MineSW.prototype.markCell = function(row, col) {
     this._doAction('mark', row, col);
@@ -15,7 +27,7 @@ MineSW.prototype.startGame = function(rowCount, colCount) {
         type: "POST",
         url: "/newGame",
         data: { row: rowCount, col: colCount },
-        success: self._processBoard()
+        success: self._processBoard
     });
 };
 
@@ -31,7 +43,7 @@ MineSW.prototype._doAction = function(action, row, col) {
 };
 
 MineSW.prototype._processBoard = function(data) {
-
+    this._renderCallback(data);
 };
 
 MineSW.prototype._gameOver  = function() {
