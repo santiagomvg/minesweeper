de8c134053f7862e7da0f5590fc864ae2d506d9e
santiagomvg <santiago.m.vidal.g@gmail.com>
2020-01-03 20:58:46 -0300

    first working version

--- static/mineSW_lib.js
+++ static/mineSW_lib.js
@@ -1,3 +1,16 @@
+const ZeroAdjacentMines = 0;
+const OneAdjacentMines = 1;
+const TwoAdjacentMines = 2;
+const ThreeAdjacentMines = 3;
+const FourAdjacentMines = 4;
+const FiveAdjacentMines = 5;
+const SixAdjacentMines = 6;
+const SevenAdjacentMines = 7;
+const EightAdjacentMines = 8;
+const Uncleared = 9;
+const UnclearedAndMarked = 10;
+const Mine = 11; //for GameOver display
+
 function MineSW() {
     //default callbacks
     this._errorCallback = function(e) { alert(e); };
@@ -5,11 +18,11 @@ function MineSW() {
 }
 
 MineSW.prototype.setRenderCallback = function(cb) {
-
+    this._renderCallback = cb;
 };
 
 MineSW.prototype.setErrorCallback = function(cb) {
-
+    this._errorCallback = cb;
 };
 
 MineSW.prototype.markCell = function(row, col) {
@@ -26,7 +39,8 @@ MineSW.prototype.startGame = function(rowCount, colCount) {
     $.ajax({
         type: "POST",
         url: "/newGame",
-        data: { row: rowCount, col: colCount },
+        data: JSON.stringify({ row: rowCount, col: colCount }),
+        contentType : 'application/json',
         success: function(data) { self._processBoard(data); }
     });
 };
@@ -37,13 +51,14 @@ MineSW.prototype._doAction = function(action, row, col) {
     $.ajax({
         type: "POST",
         url: "/action",
-        data: { action: action, row: row, col: col },
+        data: JSON.stringify({ action: action, row: row, col: col }),
+        contentType : 'application/json',
         success: function(data) { self._processBoard(data); }
     });
 };
 
 MineSW.prototype._processBoard = function(data) {
-    this._renderCallback(data);
+    this._renderCallback(JSON.parse(data));
 };
 
 MineSW.prototype._gameOver  = function() {
